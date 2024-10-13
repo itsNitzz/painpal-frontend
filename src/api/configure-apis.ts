a291d5b76b173fcd928475eb42bde5fd9dec848c
Nityanand <n.n.r1204@gmail.com>
2024-10-13 19:41:07 +0530

    fixed typo

--- src/api/configure-apis.ts
+++ src/api/configure-apis.ts
@@ -1,5 +1,2 @@
-export const FETCH_PAIN_QUESTIONS = `${(import.meta as any).env.VITE_BASE_URL || 'http://localhost:8080'}/fetch`;
-export const FETCH_PAIN_RESULT = `${(import.meta as any).env.VITE_BASE_URL || 'http://localhost:8080'}/insert`;
-
-
-
+export const FETCH_PAIN_QUESTIONS = `${(import.meta as any).env.VITE_APP_BASE_URL || "http://localhost:8080"}/fetch`;
+export const FETCH_PAIN_RESULT = `${(import.meta as any).env.VITE_APP_BASE_URL || "http://localhost:8080"}/insert`;
